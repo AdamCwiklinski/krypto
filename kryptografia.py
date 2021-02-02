4e7a96da8a2a5fa4cdb49dc599ec18f698b7d254
AdamCwiklinski <47983131+AdamCwiklinski@users.noreply.github.com>
2021-02-03 00:01:19 +0100

    moduł 4

--- kryptografia.py
+++ kryptografia.py
@@ -1,111 +1,64 @@
 import random
+import numpy as np
 from random import randrange, getrandbits, randint
 
 print("\n W python potęga to ** proszę tak wpisywać\n")
 zad = int
 
 while True:
-    zad = int(input("wpisz numer zadania: "))
+    zad = int(input("wpisz numer zadania: (1 = suma, 2 = xtime, 3 = iloczyn, 4 = odwrotnosc)"))
 
 
-    def odwrotnosc(b, n):
-        x = 0
-        xx = 1
-        while n != 0:
-            i = b // n
-            b, n = n, b - i * n
-            xx, x = x, xx - i * x
+    def suma(a, b):
+        c = hex(int(a, 16) ^ int(b, 16))
+        return c
 
-        return xx
-
-
-    def delta(y1, y2, x1, x2, p):
-        return (y1 - y2 % odwrotnosc((x1 - x2), p))
-
-    def z5(x1, x2, y1, y2, p):
-        if x1 == x2 and y1 == y2:
-            o = odwrotnosc(2 * y1, p)
-            s = (3 * x1 * x1 + a) * o
+    def mnozenie(x, y):
+        x = int(x, 16)
+        y = int(y, 16)
+        yp = y >> 1
+        if not yp:
+            return hex(x)
+        if y & 1:
+            return suma(mnozenie(xtime(hex(x)), hex(yp)), hex(x))
         else:
-            s = delta(y1, y2, x1, x2, p)
-        x = s * s - x1 - x2 % p
-        y = s * (x1 - x) - y1 % p
-        return (x,y)
+            return mnozenie(xtime(hex(x)), hex(yp))
 
 
-        def kat(p):
-            a = random.randint(1, n - 1)
-            b = random.randint(1, n - 1)
-            if ((4 * (1 ** 3)) + 27 * (b ** 2)) % p != 0:
-                return a, b
-            else:
-                return kat(p)
-        a1, b1 = kat(k)
+    def xtime(a):
+        a = int(a, 16)
+        if a & (1 << 7):
+            a = int(bin(a << 1).replace('0b', '')[-8:], 2)
+            a ^= int('0x1B', 16)
+        else:
+            a = a << 1
+        return hex(a)
 
-    if zad == 2:
-        p = eval(input("p = "))
-        a = eval(input("A = "))
-        b = eval(input("B = "))
-        x = eval(input("x = "))
-        y = eval(input("y = "))
-        n = eval(input("n = "))
-
-
-        def kat(p):
-            a = random.randint(1, n - 1)
-            b = random.randint(1, n - 1)
-            if ((4 * (1 ** 3)) + 27 * (b ** 2)) % p != 0:
-                return a, b
-            else:
-                return kat(p)
-
-
-        a1, b1 = kat(p)
-        P = "({}, {})".format(x, y)
-        x2 = x
-        y2 = y
-
-        xq, yq = z5(x, x,y,y, p)
-        n = n // 2
-        print(xq, " ", yq)
+    def odwrotnosc(x):
+        x = hex(int(x, 16))
+        y = x
+        for i in range(253):
+            y = mnozenie(x, y)
+        if y == "0x0":
+            return "undefined"
+        else:
+            return y[2:]
 
     if zad == 1:
-        k = eval(input("k = "))
-        n = k
+        a = input("a = ")
+        b = input("b = ")
+        print(suma(a, b)[2:])
 
+    if zad == 2:
+        a = input("a = ")
+        print(xtime(a)[2:])
 
+    if zad == 3:
+        a = input("a = ")
+        b = input("b = ")
+        print(mnozenie(a, b)[2:])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if zad == 4:
+        a = input("a = ")
+        print(odwrotnosc(a))
 
